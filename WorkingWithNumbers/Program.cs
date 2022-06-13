c40c1a0fe50492d02c426983aae428c634b09d2c
AyaneSHOJI <shojiayane@gmail.com>
2022-06-13 12:08:11 +0200

    p.324

--- WorkingWithNumbers/Program.cs
+++ WorkingWithNumbers/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Numerics;
 using static System.Console;
 
+
+// p.318 Numbers
 WriteLine("Worlking with large integers:");
 WriteLine("------------------------------");
 
@@ -25,3 +27,38 @@ WriteLine("{0} + {1}i aded to {2} + {3}i is {4} + {5}i",
     c2.Real, c2.Imaginary,
     c3.Real, c3.Imaginary);
 
+
+// p.320 Text
+string city = "London";
+WriteLine($"{city} is {city.Length} caracters long.");
+
+WriteLine($"First char is {city[0]} and third is {city[2]}");
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellin";
+
+string[] citiesArray = cities.Split(',');
+WriteLine($"There are {citiesArray.Length} items in the array");
+foreach(string c in citiesArray)
+{
+    WriteLine(c);
+}
+
+string fullName = "Alan Jones";
+int indexOfTheSpace = fullName.IndexOf(' '); // it returns 4
+
+string firstName = fullName.Substring(startIndex:0, length:indexOfTheSpace); // starts at 0 for length of 4
+string lastName = fullName.Substring(startIndex:indexOfTheSpace+1); // starts at 4 + 1 till the end
+
+WriteLine($"Original:{fullName}");
+WriteLine($"Swapped : {lastName}, {firstName}");
+
+string company = "Microsoft";
+bool startsWithM = company.StartsWith("M");
+bool containsN = company.Contains("N");
+WriteLine($"Text: {company} starts with M: {startsWithM}, contains an N: {containsN}");
+
+string recombined = string.Join(" => ", citiesArray);
+WriteLine(recombined);
+
+// p.324 Building strings efficiently
+// avoid String.Cancat or + operator because these methods create a completelu new string in memory
